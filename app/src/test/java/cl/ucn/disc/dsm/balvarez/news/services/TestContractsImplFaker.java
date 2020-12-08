eb20d2d17c75a3bb310c76de19d2e4efb3caab8d
Bar011 <Bar011@alumnos.ucn.cl>
2020-12-07 22:26:55 -0300

    fix(TestContractsImplFaker): fix code

--- app/src/test/java/cl/ucn/disc/dsm/balvarez/news/services/TestContractsImplFaker.java
+++ app/src/test/java/cl/ucn/disc/dsm/balvarez/news/services/TestContractsImplFaker.java
@@ -78,7 +78,7 @@ public class TestContractsImplFaker {
         Contracts contracts = new ContractsImplFaker();
 
         // Create a news
-        News news = new News("A","S","Autor", "","","aa","aa" , ZonedDateTime.now(Clock.systemUTC()));
+        News news = new News("Noticia","Fuente","Autor", "","","aa","aa" , ZonedDateTime.now(Clock.systemUTC()));
 
         // Save the news
         contracts.saveNews(news);
