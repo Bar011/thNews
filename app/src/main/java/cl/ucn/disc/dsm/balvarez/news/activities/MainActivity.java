bf7ba2aa51332aaa757bff5dfaf116ad6d499e1e
Bar011 <Bar011@alumnos.ucn.cl>
2020-12-09 00:44:27 -0300

    refactor(activities): Rename package utils to activities and add adapter

---
+++ app/src/main/java/cl/ucn/disc/dsm/balvarez/news/activities/MainActivity.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 Beatriz Alvarez-Rojas, Bar011@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package cl.ucn.disc.dsm.balvarez.news.activities;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ModelAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.balvarez.news.R;
+import cl.ucn.disc.dsm.balvarez.news.model.News;
+import cl.ucn.disc.dsm.balvarez.news.services.Contracts;
+import cl.ucn.disc.dsm.balvarez.news.services.ContractsImplNewsApi;
+
+public final class MainActivity extends AppCompatActivity {
+
+    /**
+     * The Logger.
+     */
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+    /**
+     *On create.
+     * @param savedInstanceState used to reload the app.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //the FastAdapter
+        ModelAdapter<News, NewsItem> newsAdapter = new ModelAdapter<>(NewsItem::new);
+        FastAdapter<NewsItem> fastAdapter = FastAdapter.with(newsAdapter);
+        fastAdapter.withSelectable(false);
+
+        //The recycler view
+        RecyclerView recyclerView = findViewById(R.id.am_rv_news);
+        recyclerView.setAdapter(fastAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        //Get the news in the background thread
+        AsyncTask.execute(() -> {
+
+            //Using the contracts to get the news...
+            Contracts contracts = new ContractsImplNewsApi("...");
+
+            //Get the News from the NewsApi( Internet!)
+            List<News> listNews = contracts.retrieveNews(30);
+
+            //Set the adapter!
+            runOnUiThread(() -> {
+                newsAdapter.add(listNews);
+            });
+        });
+
+
+        }
+
+    }
+
