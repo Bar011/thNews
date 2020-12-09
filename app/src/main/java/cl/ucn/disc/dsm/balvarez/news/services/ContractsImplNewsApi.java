54dd91708ee7b437cfae4db002de476e323d8ad9
Bar011 <Bar011@alumnos.ucn.cl>
2020-12-08 21:19:27 -0300

    fix(NewsApi): remove duplicates

--- app/src/main/java/cl/ucn/disc/dsm/balvarez/news/services/ContractsImplNewsApi.java
+++ app/src/main/java/cl/ucn/disc/dsm/balvarez/news/services/ContractsImplNewsApi.java
@@ -23,6 +23,11 @@ import org.threeten.bp.ZonedDateTime;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import cl.ucn.disc.dsm.balvarez.news.model.News;
 import cl.ucn.disc.dsm.balvarez.news.utils.Validation;
@@ -59,7 +64,7 @@ public class ContractsImplNewsApi implements Contracts {
             boolean needFix = false;
 
             // Fix the author null
-            if (article.getAuthor() == null) {
+            if (article.getAuthor() == null || article.getAuthor().length() == 0) {
 
                 article.setAuthor("No author");
                 needFix = true;
@@ -100,6 +105,18 @@ public class ContractsImplNewsApi implements Contracts {
             );
         }
 
+    /**
+     *Filter the stream.
+     *
+     * @param idExtractor
+     * @param <T> news to filter
+     * @return true if the news already exist.
+     */
+        private static <T> Predicate<T> distintByid(Function<? super T, ?> idExtractor){
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(idExtractor.apply(t), Boolean.TRUE==null);
+    }
+
         /**
          * Get the list of News
          *
@@ -120,11 +137,19 @@ public class ContractsImplNewsApi implements Contracts {
                     // log.debug("Article: {}", ToStringBuilder.reflectionToString(article, ToStringStyle.MULTI_LINE_STYLE));
                     news.add(toNews(article));
                 }
-                return news;
-            } catch (IOException ex) {
 
-                log.error("Error: ", ex);
-                return null;
+                //Filter and sort the News
+                return news.stream()
+                        //Remote the duplicates (by id)
+                        .filter(distintByid(News::getId))
+                        //Sort the stream by PublishedAt
+                        .sorted((k1, k2)->k2.getPublishedAt().compareTo(k1.getPublishedAt()))
+                        //Return the stream to list
+                        .collect(Collectors.toList());
+
+            } catch (IOException ex) {
+                //Encapsulate!
+                throw new RuntimeException(ex);
             }
         }
 
